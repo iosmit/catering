3b586510bebaecabd00f3cbff17d804c698f8818
Smit <69114262+iosmit@users.noreply.github.com>
2024-01-10 15:22:45 -0500

    Add files via upload

--- Resources/script/lightbox.js
+++ Resources/script/lightbox.js
@@ -1,5 +1,8 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+
+//Get the base page
+var base=document.getElementById("hidePage");
   
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -9,12 +12,14 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
+  base.style.display="none";
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  base.style.display="block";
 }
 
 // When the user clicks anywhere outside of the modal, close it
