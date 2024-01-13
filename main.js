716432a828677aae22a6718ebfc855ac3a377ba9
Smit <69114262+iosmit@users.noreply.github.com>
2024-01-12 23:29:05 -0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("serviceWorker.js")
-      .then((reg) => console.log("The service worker is registered:", reg))
-      .catch((err) => console.log("Ops, we got an error:  " + err));
-  });
-}
